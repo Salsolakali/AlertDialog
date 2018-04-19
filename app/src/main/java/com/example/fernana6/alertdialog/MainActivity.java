9b4579ff50e2082d2d4208275593fa00070da8b5
EU\FernanA6 <alberto.fernandezseral@unisys.com>
2018-04-19 15:27:19 +0200

    Con lista seleccionable

--- app/src/main/java/com/example/fernana6/alertdialog/MainActivity.java
+++ app/src/main/java/com/example/fernana6/alertdialog/MainActivity.java
@@ -11,41 +11,82 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button btnAlert;
+    Button btnAlert, btnList;
     Context context = this;
+    ArrayList mSelectedItems = new ArrayList();
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        btnAlert=findViewById(R.id.btnAlertDialog);
+        btnAlert = findViewById(R.id.btnAlertDialog);
+        btnList = findViewById(R.id.AlertDialogList);
+
         btnAlert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
                 alertDialogBuilder.setTitle("Titulo");
                 alertDialogBuilder.setMessage("Mensaje")
 
-                    .setPositiveButton(R.string.aceptar, new DialogInterface.OnClickListener(){
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            Toast toast = Toast.makeText(context,R.string.aceptar,Toast.LENGTH_LONG);
-                            toast.show();
-                        }
-                    })
-                    .setNegativeButton(R.string.cancelar, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            Toast toast = Toast.makeText(context,R.string.cancelar,Toast.LENGTH_LONG);
-                            toast.show();
-                        }
-                    });
+                        .setPositiveButton(R.string.aceptar, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast toast = Toast.makeText(context, R.string.aceptar, Toast.LENGTH_SHORT);
+                                toast.show();
+                            }
+                        })
+                        .setNegativeButton(R.string.cancelar, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast toast = Toast.makeText(context, R.string.cancelar, Toast.LENGTH_SHORT);
+                                toast.show();
+                            }
+                        });
                 AlertDialog alertDialog = alertDialogBuilder.create();
 
                 alertDialog.show();
 
+            }
+        });
+
+        btnList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alertListDialogBuilder = new AlertDialog.Builder(context);
+                mSelectedItems.removeAll(mSelectedItems);
+                alertListDialogBuilder.setTitle(R.string.colores)
+                        .setMultiChoiceItems(R.array.colores, null, new DialogInterface.OnMultiChoiceClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i, boolean b) {
+                                if (b) {
+                                    mSelectedItems.add(i);
+                                } else if (mSelectedItems.contains(i)) {
+                                    mSelectedItems.remove(Integer.valueOf(i));
+                                }
+                            }
+                        })
+                        .setPositiveButton(R.string.aceptar, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast toast = Toast.makeText(context, mSelectedItems.toString(), Toast.LENGTH_SHORT);
+                                toast.show();
+                            }
+                        })
+                        .setNegativeButton(R.string.cancelar, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast toast = Toast.makeText(context, R.string.cancelar, Toast.LENGTH_SHORT);
+                                toast.show();
+                            }
+                        });
+                alertListDialogBuilder.create().show();
             }
         });
     }
